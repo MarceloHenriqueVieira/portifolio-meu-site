3cec661e22bae5fc2cf4fc03886407830688b63b
MarceloHenriqueVieira <74198183+MarceloHenriqueVieira@users.noreply.github.com>
2022-03-27 12:38:42 -0300

    Commit inical

---
+++ script.js
@@ -0,0 +1,10 @@
+function troca(){
+  var text=document.getElementById("texto")
+  text.innerText="Depois";
+
+}
+function trocas(){
+  var texto=document.getElementById("texto")
+  texto.innerText="Antes";
+
+}
